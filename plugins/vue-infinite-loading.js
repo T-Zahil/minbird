5ba139c9facbda3c33a37337f8fd7c64b3f617bc
Thomas Sanlis <thomas.sanlis@gmail.com>
2020-10-30 14:23:09 +0100

    Add throttleLimit parameter to infinite loading plugin

--- plugins/vue-infinite-loading.js
+++ plugins/vue-infinite-loading.js
@@ -2,4 +2,8 @@ import Vue from 'vue'
 
 import InfiniteLoading from 'vue-infinite-loading'
 
-Vue.use(InfiniteLoading, {})
+Vue.use(InfiniteLoading, {
+  system: {
+    throttleLimit: 500,
+  },
+})
