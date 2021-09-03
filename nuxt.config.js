b0dd92034a770a23b2e803c2d3fee2f484248e03
Thomas Sanlis <thomas.sanlis@gmail.com>
2021-09-03 16:55:17 +0200

    Upgrade axios

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,49 +9,49 @@ export default {
     script: [
       {
         async: 'async',
-        src: 'https://cdn.splitbee.io/sb.js',
-      },
+        src: 'https://cdn.splitbee.io/sb.js'
+      }
     ],
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: '' }
     ],
     link: [
       {
         rel: 'icon',
         type: 'image/x-icon',
-        href: '/favicon.ico',
+        href: '/favicon.ico'
       },
       {
         rel: 'apple-touch-icon',
         href: '/apple-touch-icon.png',
-        sizes: '180x180',
+        sizes: '180x180'
       },
       {
         rel: 'apple-touch-icon',
         href: '/apple-touch-icon.png',
-        sizes: '180x180',
+        sizes: '180x180'
       },
       {
         rel: 'icon',
         type: 'image/png',
         href: '/favicon-32x32.png',
-        sizes: '32x32',
+        sizes: '32x32'
       },
       {
         rel: 'icon',
         type: 'image/png',
         href: '/favicon-16x16.png',
-        sizes: '16x16',
-      },
-    ],
+        sizes: '16x16'
+      }
+    ]
   },
 
   env: {
     TWITTER_API_KEY: process.env.TWITTER_API_KEY,
     TWITTER_SECRET_KEY: process.env.TWITTER_SECRET_KEY,
-    TWITTER_BEARER: process.env.TWITTER_BEARER,
+    TWITTER_BEARER: process.env.TWITTER_BEARER
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
@@ -61,7 +61,7 @@ export default {
   plugins: ['~/plugins/vue-infinite-loading.js'],
 
   colorMode: {
-    preference: 'sepia',
+    preference: 'sepia'
   },
 
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
@@ -72,14 +72,14 @@ export default {
     '@nuxtjs/stylelint-module',
     '@nuxtjs/color-mode',
     '@nuxtjs/tailwindcss',
-    '@nuxtjs/svg',
+    '@nuxtjs/svg'
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
-    'nuxt-vuex-localstorage',
+    'nuxt-vuex-localstorage'
   ],
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
@@ -88,10 +88,10 @@ export default {
       'Content-Type': 'application/json',
       'content-type': 'application/json',
       'Cache-Control': 'no-cache',
-      'X-Accel-Buffering': 'no',
-    },
+      'X-Accel-Buffering': 'no'
+    }
   },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {},
+  build: {}
 }
